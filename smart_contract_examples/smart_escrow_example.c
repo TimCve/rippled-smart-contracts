eba5c87fa943f1d5d3a51a98cfc3b26398892d19
TimCve <timofey.cvetkov@protonmail.com>
2026-02-11 20:46:22 +0000

    correct variable naming in smart_escrow_example

--- smart_contract_examples/smart_escrow_example.c
+++ smart_contract_examples/smart_escrow_example.c
@@ -9,14 +9,14 @@ typedef struct PACKED
 
 typedef struct PACKED
 {
-    id256_t state_id;
+    id256_t escrow_id;
 } params2_t;
 
 typedef struct PACKED
 {
     addr_t recipient;
     uint32_t amount;
-} state_t;
+} escrow_t;
 
 __attribute__((export_name("entrypoint")))
 int32_t entrypoint(int64_t opt)
@@ -29,14 +29,14 @@ int32_t entrypoint(int64_t opt)
         int32_t got = getParams((int32_t)&params, (int32_t)sizeof(params1_t));
         if(got != (int32_t)sizeof(params1_t)) return -2;
         
-        state_t state;
-        state.recipient = params.recipient;
-        state.amount = params.amount;
+        escrow_t escrow;
+        escrow.recipient = params.recipient;
+        escrow.amount = params.amount;
         if(lockCallerXRP((int32_t)params.amount) != 0)
             return -3;
 
-        id256_t state_id;
-        if(createSmartObject((int32_t)&state, (int32_t)sizeof(state_t), (int32_t)&state_id) != 0)
+        id256_t escrow_id;
+        if(createSmartObject((int32_t)&escrow, (int32_t)sizeof(escrow_t), (int32_t)&escrow_id) != 0)
             return -4;
 
     // opt = 2 : claim
@@ -45,19 +45,19 @@ int32_t entrypoint(int64_t opt)
         int32_t got = getParams((int32_t)&params, (int32_t)sizeof(params2_t));
         if(got != (int32_t)sizeof(params2_t)) return -5;
 
-        state_t state;
-        int32_t sz = getSmartObject((int32_t)&params.state_id, (int32_t)&state, sizeof(state_t));
-        if(sz != (int32_t)sizeof(state_t)) return -6;
+        escrow_t escrow;
+        int32_t sz = getSmartObject((int32_t)&params.escrow_id, (int32_t)&escrow, sizeof(escrow_t));
+        if(sz != (int32_t)sizeof(escrow_t)) return -6;
         
         addr_t caller;
         if(getCallerAddr((int32_t)&caller) != 0)
             return -7;
 
-        if(MEMEQ(&caller, &state.recipient, ADDR_SIZE)) {
-            if(unlockXRP((int32_t)state.amount, (int32_t)&caller) != 0)
+        if(MEMEQ(&caller, &escrow.recipient, ADDR_SIZE)) {
+            if(unlockXRP((int32_t)escrow.amount, (int32_t)&caller) != 0)
                 return -9;
             
-            if(deleteSmartObject((int32_t)&params.state_id) != 0)
+            if(deleteSmartObject((int32_t)&params.escrow_id) != 0)
                 return -10;
         } else return -8;
     } else return -11;
